ff67df16b8b72d510446fd2a3ef371ee7c9105c6
hliosone <sstelcher@protonmail.com>
2023-05-19 19:42:29 +0200

    Added comments on each Smart Contract functions

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -8,14 +8,17 @@ contract StorageFactory {
     //So default value is 0x000.......
     SimpleStorage[] public simpleStorage;
 
+    //push the address of new SCs in our array
     function createSimpleContract() public {
         simpleStorage.push(new SimpleStorage());
     }
 
+    //store a number using function from other SC in a contract
     function sfStore(uint256 _storageIndex, uint256 storageNumber) public {
         simpleStorage[_storageIndex].store(storageNumber);
     }
 
+    //getter for a stored number in a contract
     function sfRetrieve(uint256 _storageIndex) public view returns (uint256){
         return simpleStorage[_storageIndex].retrieve();
     }
